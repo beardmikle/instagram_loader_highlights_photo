4fea5663569899fa58639884e27f256b670b746c
beardmikle <ankmo@yandex,ru>
2022-05-14 02:57:44 +0300

    2

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ L.login(USER, PASSWORD)
 account = input('Введите имя аккаунта для скачивания:')
 
 L.download_profile(account, profile_pic=True, profile_pic_only=True, fast_update=True, download_stories=True, download_stories_only=True, download_tagged=True, download_tagged_only=True, post_filter=None, storyitem_filter=None)
-# L.download_highlights(self,
-                            user: Union[int, Profile],
-                            fast_update: bool = False,
-                            filename_target: Optional[str] = None,
-                            storyitem_filter: Optional[Callable[[StoryItem], bool]] = None)
+# # L.download_highlights(self,
+#                             user: Union[int, Profile],
+#                             fast_update: bool = False,
+#                             filename_target: Optional[str] = None,
+#                             storyitem_filter: Optional[Callable[[StoryItem], bool]] = None)
