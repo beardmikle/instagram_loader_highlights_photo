14505c1ee60729707c01a074cc3e3431c6b44566
beardmikle <ankmo@yandex,ru>
2022-05-14 05:30:20 +0300

    1

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ instance = instaloader.Instaloader()
 
 instance.login(user=input("Введите ваш логин:"),passwd=input("Введите ваш пароль:"))
 
-profile = Profile.from_username(instance.context, username=input("Введите аккаунт для скачивания историй:"))
+profile = Profile.from_username(instance.context, username=input("Введите аккаунт для скачивания:"))
 
 for highlight in instance.get_highlights(user=profile):
     for item in highlight.get_items():
         instance.download_storyitem(item, '{}/{}'.format(highlight.owner_username, highlight.title))
 
-instance.download_profile(profile_name=input("Введите аккаунт для скачивания остального:"))
+instance.download_profile(profile_name=profile)
 
